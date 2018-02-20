f13093c820921bae18ef9e3d8839d5021005047b
heatherhornor <35786024+heatherhornor@users.noreply.github.com>
2018-02-20 14:05:58 -0600

    Add files via upload

---
+++ final_project/chesswebsite/js/main.js
@@ -0,0 +1,8 @@
+function magicnav() {
+    var x = document.getElementById("nav");
+    if (x.style.display === "none") {
+        x.style.display = "block";
+    } else {
+        x.style.display = "none";
+    }
+}
