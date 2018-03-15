5cb4a24003a1ff1bf2bf4e7d2d7bd25cfbf00c40
heatherhornor <35786024+heatherhornor@users.noreply.github.com>
2018-03-15 14:51:34 -0500

    Add files via upload

--- final_project/chesswebsite/js/main.js
+++ final_project/chesswebsite/js/main.js
@@ -1,8 +1,41 @@
-function magicnav() {
-    var x = document.getElementById("nav");
-    if (x.style.display === "none") {
-        x.style.display = "block";
-    } else {
-        x.style.display = "none";
-    }
-}
+// (window).on('scroll', function () {
+// 	var distanceScrolled = $(window).scrollTop();
+//
+//   if(distanceScrolled> 50) {
+//     $('#arrowbutton').addClass(arrowBg);
+//   }
+// });
+$('.clubQ1').on('click', function () {
+	$('.modal-background1').css("display", "flex").hide().fadeIn();
+});
+
+$('.clubQ2').on('click', function () {
+	$('.modal-background2').css("display", "flex").hide().fadeIn();
+});
+
+$('#close1').on('click', function () {
+	$('.modal-background1').fadeOut();
+});
+
+$('#close2').on('click', function () {
+	$('.modal-background2').fadeOut();
+});
+
+$('#arrowbutton').on('click', function() {
+  $(".bigNav").slideToggle(300);
+});
+
+$(window).on('scroll', function () {
+	var distanceScrolled = $(window).scrollTop();
+
+  if(distanceScrolled> 100) {
+    $('header').addClass('littleHead');
+    $('nav').addClass('navOpac');
+    $('.container1').addClass('containerSlide');
+  }
+  // else {
+  //   $('header').removeClass('littleHead');
+  //   $('nav').removeClass('navOpac');
+  //   $('.container1').removeClass('containerSlide');
+  // }
+});
